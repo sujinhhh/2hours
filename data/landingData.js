285a9ce2f6298104161e8b202731f141288cf6d1
Sujin Lee <ccclalasj@gmail.com>
2021-07-14 14:15:13 -0400

    deploy

--- data/landingData.js
+++ data/landingData.js
@@ -17,17 +17,17 @@ const imageData = [
 ];
 const videoData = [
   {
-    videoUrl:
+    mediaUrl:
       "https://player.vimeo.com/external/380034348.sd.mp4?s=6cbf774477045e18bce4a4b3097199e75b84bc7c&profile_id=165&oauth2_token_id=57447761",
     title: "Ohahah",
   },
   {
-    videoUrl:
+    mediaUrl:
       "https://player.vimeo.com/external/195912468.sd.mp4?s=4fdf320d5912bcfc67fcdb40d85a75ed38c2285a&profile_id=164&oauth2_token_id=57447761",
     title: "Ohahah",
   },
   {
-    videoUrl:
+    mediaUrl:
       "https://player.vimeo.com/external/497762976.sd.mp4?s=f7f16e08de0711803f3053df4ba29ac88568eac7&profile_id=165&oauth2_token_id=57447761",
     title: "Ohahah",
   },
