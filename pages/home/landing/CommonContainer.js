d74292ae97a7a8f1adf942614df31d30071faa52
Sujin Lee <ccclalasj@gmail.com>
2021-07-30 18:35:17 -0400

    hero

--- pages/home/landing/CommonContainer.js
+++ pages/home/landing/CommonContainer.js
@@ -7,23 +7,23 @@ const CommonContainer = ({ position }) => {
   const imageData = [
     {
       mediaUrl:
-        "https://images.unsplash.com/photo-1555573970-48691be3d9d9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fHNwcmluZ3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-      title: "Spring",
+        "https://images.unsplash.com/photo-1473496169904-658ba7c44d8a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3VtbWVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
+      title: "SMILE",
     },
     {
       mediaUrl:
         "https://images.unsplash.com/photo-1567653418876-5bb0e566e1c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
-      title: "Summer",
+      title: "JS/REACT",
     },
     {
       mediaUrl:
-        "https://images.unsplash.com/photo-1614481327033-68e5df399653?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=951&q=80",
-      title: "Fall",
+        "https://images.unsplash.com/photo-1499561385668-5ebdb06a79bc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTE4fHxzdW1tZXJ8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+      title: "HAPPY",
     },
     {
       mediaUrl:
-        "https://images.unsplash.com/photo-1519751138087-5bf79df62d5b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHdpbnRlcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-      title: "Winter",
+        "https://images.unsplash.com/uploads/14121010130570e22bcdf/e1730efe?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHN1bW1lcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
+      title: "CSS/HTML",
     },
   ];
   const first = imageData[1];
