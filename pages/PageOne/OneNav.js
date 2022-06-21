46df0e64e5f70a9cee3743573300e9664fb9d840
Sujin Lee <ccclalasj@gmail.com>
2022-06-21 16:59:00 -0400

    resume

--- pages/PageOne/OneNav.js
+++ pages/PageOne/OneNav.js
@@ -1,20 +1,35 @@
 import React from "react";
+import Link from "next/link";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 function OneNav() {
   return (
     <div className="one-nav-container">
       <ul className="one-menu">
-        {/* <li> Resume</li>
-        <li> Cover letter</li>
+        <a href="./bmo.pdf" target="_blank">
+          <li> Resume</li>
+        </a>
+        {/* <li> Cover letter</li> */}
         <li>
-          <AiFillGithub size="30px" />
-          <AiFillLinkedin
-            size="30px"
-            color="#0b65c2"
-            style={{ marginLeft: 10 }}
-          />
-        </li> */}
+          <a
+            href="https://github.com/sujinhhh"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillGithub size="30px" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sujin-lee-26694820b/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillLinkedin
+              size="30px"
+              color="#0b65c2"
+              style={{ marginLeft: 10 }}
+            />
+          </a>
+        </li>
       </ul>
     </div>
   );
